58ce5ae942bbb433a2551ae8e47c090a18b25b04
ricardo.amorim3@gmail.com <ricardo.amorim3@gmail.com>
2015-12-06 23:36:57 +0000

    Updated about section text

--- app/src/main/java/pt/up/fe/infolab/ricardo/antmobile/fragments/AboutFragment.java
+++ app/src/main/java/pt/up/fe/infolab/ricardo/antmobile/fragments/AboutFragment.java
@@ -24,14 +24,26 @@ public class AboutFragment extends Fragment {
 
         final View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
-        String about = "<p>O ANT Mobile é uma aplicação para explorar recursos no sistema de informação da Univesridade do Porto. Foi desenvolvida por Ricardo Amorim (paginas.fe.up.pt/~rcamorim) no seu tempo livre como forma" +
-                "de se manter a par das novidades do mundo da programação móvel" +
-                "Foi desenvolvido para permitir o acesso simplificado à informação disponibilizada nos vários sistemas de informação da universidade.</p>" +
-                "<p>Toda a informação apresentada é extraída a partir da plataforma ANT (ant.fe.up.pt). Esta plataforma é o fruto de uma bolsa de investigação a " +
-                "cargo de José Devezas (josedevezas.com). Atualmente, é utilizado como caso de estudo central o sistema de informação SIGARRA, sendo, contudo, o " +
-                "nosso objetivo evoluir para um sistema modular e mais genérico, criando ramificações em várias subáreas de investigação, permitindo contribuições adicionais" +
-                " aplicadas ao nosso caso de estudo em particular, mas também experimentações diretamente suportadas pela nossa plataforma (o ANT Mobile é um exemplo concreto " +
-                "destas experimentações).</p>";
+        String about = "<p>O ANT Mobile é uma aplicação para explorar recursos no sistema de informação da Univesridade do Porto. Foi desenvolvida " +
+                "por dois programadores entusiastas:</p>\n" +
+                "&#8226; <b>Ricardo Amorim (paginas.fe.up.pt/~rcamorim)</b> um programador júnior focado em criar aplicações móveis que rapidamente" +
+                " resolvam problemas do dia a dia, com uma pitada de bom design (design não é claramente o seu forte). Tem um fraquinho por chocolate" +
+                " negro, e vibra com jantaradas e viagens. <br/>\n" +
+                "&#8226; <b>José Devezas</b> Autor da plataforma ANT, o José tem uma grande experiência em processamento de informação, e consegue " +
+                "reunir numa pessoa o know-how em diversas áreas desde a geometria até à análise de dados. Enfim um informático canivete suíço, como" +
+                " qualquer empresa procura.<br/>\n" +
+                "<p>São então estes os dois singelos programadores que decidiram pegar no problema do acesso a informações do SIGARRA (e da " +
+                "inexistência de uma aplicação bonita o suficiente para este efeito) e tentar resolve-lo. Ora, o ANT e o ANT Mobile são parte da " +
+                "solução, sendo que o ANT representa aqui a fonte de 99% da informação apresentada no ANT Mobile (não, o 1% em falta não é informação" +
+                " de tracking. Corresponde apenas à ocupação do parque que vem de outro sítio (confidencial).</p>" +
+                "<p>O ANT tem uma rotina que recolhe, processa e indexa os dados disponíveis no sistema de informação (no domínio público) e por isso" +
+                " mesmo consegue resultados de pesquisa consideravelmente mais rápido. Além disso, tem mecanismos que procuram reconhecer entidades" +
+                " como professor e aluno para encontrar relações entre indivíduos. Para saber mais, nada melhor do que consultar a página do ant " +
+                "(ant.fe.up.pt).</p>" +
+                "<br/><p> O nosso objetivo é que esta abordagem possa ser aplicada para obter informação a partir de qualquer sistema de informação " +
+                "(o SIGARRA foi o exemplo de teste). Claro que não podíamos deixar features valiosas de lado e por isso decidimos incluir cantinas " +
+                "e parques.</p>"
+                ;
                 ((TextView) rootView.findViewById(R.id.text_about)).setText(Html.fromHtml(about));
 
         return rootView;
